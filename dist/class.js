19f7252c296cc8dad6ffc421209c2c23ebea9af8
AbdulMunim343 <munimwork90@gmail.com>
2023-03-28 23:43:37 +0500

    new class oops

--- dist/class.js
+++ dist/class.js
@@ -1,18 +1,64 @@
 "use strict";
-class Student {
-    constructor(name, rollNo) {
-        this.name = name;
-        this.rollNo = rollNo;
-        // name: string;
-        // rollNo: number;
-        // short hand
-        this.skill = [];
-    }
-    addSkills(skills) {
-        this.skill.push(skills);
+// class Student {
+//     // name: string;
+//     // rollNo: number;
+//     // short hand
+//     private skill: string[] = [];
+//     constructor(public name: string, public readonly rollNo: number) { }
+//     addSkills(skills: string) {
+//         this.skill.push(skills);
+//     }
+// }
+// const student1 = new Student("Yasir", 321);
+// student1.addSkills("JavaScript")
+// console.log(student1)
+//inheritance class and abstrac methods
+// abstract class Product {
+//     constructor(
+//         private _id: number,
+//         private _name: string,
+//         private _price: number) { }
+//     get id() {
+//         return this._id;
+//     }
+//     get name() {
+//         return this._name;
+//     }
+//     set name(newName: string) {
+//         if (!newName) {
+//             throw new Error("Name cannot be empty");
+//         }
+//         this._name = newName;
+//     }
+//     get price() {
+//         return this._price;
+//     }
+//     abstract getDiscount():number
+// }
+// class Clothing extends Product{
+//     constructor(
+//         id:number,
+//         name:string,
+//         price:number,
+//         private color:string,
+//         private size:"M" | "L" |"XL"
+//         ){
+//             super(id,name,price)
+//         }
+//         getDiscount(){
+//            return this.price *0.9;
+//         }
+// }
+// const tShirt = new Clothing(1,"T-Shirt",1000,"Blue","M");
+// console.log(tShirt)
+class Util {
+    constructor() { }
+    static getInstance() {
+        if (this.Instance) {
+            this.Instance = new Util();
+        }
+        return this.Instance;
     }
 }
-const student1 = new Student("Yasir", 321);
-student1.addSkills("JavaScript");
-console.log(student1);
+const utlInc = Util.getInstance();
 //# sourceMappingURL=class.js.map
--- src/class.ts
+++ src/class.ts
@@ -1,16 +1,82 @@
-class Student {
-    // name: string;
-    // rollNo: number;
+// class Student {
+//     // name: string;
+//     // rollNo: number;
 
-    // short hand
-    private skill: string[] = [];
-    constructor(public name: string, public readonly rollNo: number) { }
+//     // short hand
+//     private skill: string[] = [];
+//     constructor(public name: string, public readonly rollNo: number) { }
 
-    addSkills(skills: string) {
-        this.skill.push(skills);
+//     addSkills(skills: string) {
+//         this.skill.push(skills);
+//     }
+// }
+
+// const student1 = new Student("Yasir", 321);
+// student1.addSkills("JavaScript")
+// console.log(student1)
+
+
+//inheritance class and abstrac methods
+// abstract class Product {
+//     constructor(
+//         private _id: number,
+//         private _name: string,
+//         private _price: number) { }
+
+
+//     get id() {
+//         return this._id;
+//     }
+
+//     get name() {
+//         return this._name;
+//     }
+
+//     set name(newName: string) {
+//         if (!newName) {
+//             throw new Error("Name cannot be empty");
+//         }
+
+//         this._name = newName;
+//     }
+
+//     get price() {
+//         return this._price;
+//     }
+
+//     abstract getDiscount():number
+// }
+
+// class Clothing extends Product{
+//     constructor(
+//         id:number,
+//         name:string,
+//         price:number,
+//         private color:string,
+//         private size:"M" | "L" |"XL"
+//         ){
+//             super(id,name,price)
+//         }
+
+//         getDiscount(){
+//            return this.price *0.9;
+//         }
+// }
+
+// const tShirt = new Clothing(1,"T-Shirt",1000,"Blue","M");
+// console.log(tShirt)
+
+class Util {
+    private static Instance: Util;
+    private constructor() { }
+
+    static getInstance() {
+        if (this.Instance) {
+            this.Instance = new Util();
+        }
+
+        return this.Instance;
     }
 }
 
-const student1 = new Student("Yasir", 321);
-student1.addSkills("JavaScript")
-console.log(student1)
+const utlInc = Util.getInstance();
